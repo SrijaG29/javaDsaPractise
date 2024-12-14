660e84f23abb0265ee9208313f8ad5ff8c30c03a
SrijaG29 <srijag298@gmail.com>
2024-12-14 13:46:55 +0530

    checkIfArrayIsSortedAndRotated commit

--- src/Arrays/checkIfArrayIsSortedAndRotated.java
+++ src/Arrays/checkIfArrayIsSortedAndRotated.java
@@ -2,7 +2,8 @@ package Arrays;
 
 public class checkIfArrayIsSortedAndRotated {
     public static void main(String[] args){
-        int[] nums = {3,4,5,1,2};
+//        int[] nums = {3,4,5,1,2};
+        int[] nums = {3,4,1,2};
         int count =0 ;
         for(int i=0;i<nums.length-1;i++){
             if(nums[i]>nums[i+1]){
